ceac00f0b6f6f543f41863a3af5a8c1ca1c33b60
Artur <artur2005456@gmail.com>
2025-04-02 21:05:08 +0300

    add

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sqlalchemy import text
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
