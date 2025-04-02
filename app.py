9f6d234f8b1aca37e36ffb0810acafab2efefa83
Artur <artur2005456@gmail.com>
2025-04-02 18:44:36 +0300

    Add categories

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from sqlalchemy import text
 
 app = Flask(__name__)
@@ -8,6 +9,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://animetracker_db:123456789@
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+from models import Category
+
+migrate = Migrate(app, db)
 
 @app.route("/")
 def index():
@@ -22,4 +27,6 @@ def test_db():
         return f"Error: {str(e)}"
 
 if __name__ == '__main__':
+    with app.app_context():
+      db.create_all()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,9 +1,14 @@
+from app import db
 from flask_sqlalchemy import SQLAlchemy
+from slugify import slugify
 
-db = SQLAlchemy()
 
 class Category(db.Model):
     __tablename__ = 'category'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     slug = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+        self.slug = slugify(name)
