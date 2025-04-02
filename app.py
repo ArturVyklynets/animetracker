a7a15f882cfbba3b7b39887b01a85fe988d243a8
Artur <artur2005456@gmail.com>
2025-04-02 13:44:54 +0300

    Connect database

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://animetracker_db:123456789@localhost:5433/animeTracker"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/test-db")
+def test_db():
+    try:
+        result = db.session.execute(text('SELECT 1'))
+        return "Database connection is working!"
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 if __name__ == '__main__':
     app.run(debug=True)
