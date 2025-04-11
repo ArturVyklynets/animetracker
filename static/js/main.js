97cac444c8cb8b55a68796c93afad7cc470072b3
Andriy_Boychuk <Andrii.boichuk.22@pnu.edu.ua>
2025-04-11 20:50:29 +0300

    update sliders in index.html

--- static/js/main.js
+++ static/js/main.js
@@ -22,74 +22,39 @@ moviePrevBtn.addEventListener('click', () => {
 showMovieSlide(movieIndex);
 
 // Слайдери для Рекомендації
-  // const items = document.querySelectorAll('.item');
-  // const nextBtn = document.getElementById('nextBtn');
-  // const prevBtn = document.getElementById('prevBtn');
+  const items = document.querySelectorAll('.item');
+  const nextBtn = document.getElementById('nextBtn');
+  const prevBtn = document.getElementById('prevBtn');
 
-  // const itemsPerPage = 4;
-  // let currentIndex = 0;
+  const itemsPerPage = 4;
+  let currentIndex = 0;
 
-  // function updateVisibleItems() {
-  //   items.forEach(item => item.classList.remove('show'));
+  function updateVisibleItems() {
+    items.forEach(item => item.classList.remove('show'));
 
-  //   for (let i = currentIndex; i < currentIndex + itemsPerPage; i++) {
-  //     if (items[i]) {
-  //       items[i].classList.add('show');
-  //     }
-  //   }
-  // }
+    for (let i = currentIndex; i < currentIndex + itemsPerPage; i++) {
+      if (items[i]) {
+        items[i].classList.add('show');
+      }
+    }
+  }
 
-  // nextBtn.addEventListener('click', () => {
-  //   currentIndex += itemsPerPage;
-  //   if (currentIndex >= items.length) {
-  //     currentIndex = 0;
-  //   }
-  //   updateVisibleItems();
-  // });
+  nextBtn.addEventListener('click', () => {
+    currentIndex += itemsPerPage;
+    if (currentIndex >= items.length) {
+      currentIndex = 0;
+    }
+    updateVisibleItems();
+  });
 
-  // prevBtn.addEventListener('click', () => {
-  //   currentIndex -= itemsPerPage;
-  //   if (currentIndex < 0) {
-  //     // Переходимо до останнього повного набору
-  //     currentIndex = Math.max(0, items.length - itemsPerPage);
-  //   }
-  //   updateVisibleItems();
-  // });
+  prevBtn.addEventListener('click', () => {
+    currentIndex -= itemsPerPage;
+    if (currentIndex < 0) {
+      // Переходимо до останнього повного набору
+      currentIndex = Math.max(0, items.length - itemsPerPage);
+    }
+    updateVisibleItems();
+  });
 
-  // // Показати перші
-  // updateVisibleItems();
-
-  const container = document.getElementById('container');
-
-let isDown = false;
-let startX;
-let scrollLeft;
-
-container.addEventListener('mousedown', (e) => {
-  isDown = true;
-  container.classList.add('active'); // для стилів grab
-  startX = e.pageX - container.offsetLeft;
-  scrollLeft = container.scrollLeft;
-});
-
-container.addEventListener('mouseleave', () => {
-  isDown = false;
-  container.classList.remove('active');
-});
-
-container.addEventListener('mouseup', () => {
-  isDown = false;
-  container.classList.remove('active');
-});
-
-container.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - container.offsetLeft;
-  const walk = (x - startX) * 1.5; // коефіцієнт швидкості
-  container.scrollLeft = scrollLeft - walk;
-});
-
-
-
-  
+  // Показати перші
+  updateVisibleItems();
