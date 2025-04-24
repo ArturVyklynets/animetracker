520a4aa04438d7cd9a95636118b9b54ad81b5b31
Artur <artur2005456@gmail.com>
2025-04-25 00:54:19 +0300

    fd

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-uri = os.getenv("DATABASE_URL")
+uri = os.getenv("DATABASE_URL") or "postgres://u6mcj67k662hjg:pce7faeb7ed90e726c97bf6772ee7975a053396e15cc47aafb393465c6361acf6@c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/deiqmb1se9nph"
 if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
